b82200db1b8d0dea7d0f117b514a3c8b210799f0
Jeremy Clements <jeremy.clements@lexisnexisrisk.com>
2019-02-12 17:04:00 -0500

    added route to users router for fetching a user's workspaces

--- app/routes/users.js
+++ app/routes/users.js
@@ -1,9 +1,30 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+
+const db = require('../models/index');
+const User = db.User;
+const Workspace = db.Workspace;
+const WorkspaceUser = db.WorkspaceUser;
 
 /* GET users listing. */
-router.get('/account', function(req, res, next) {
+router.get('/account', (req, res, next) => {
   res.render('users/account', { title: 'ECL IDE' });
+});
+
+router.get('/workspaces', (req, res, next) => {
+  User.findByPk(req.session.user.id, {
+    include: [{
+      model: Workspace,
+      through: {
+        where: { role: WorkspaceUser.roles.OWNER }
+      }
+    }]
+  }).then((user) => {
+    res.json(user.Workspaces);
+  }).catch((err) => {
+    console.log(err);
+    res.json(err);
+  });
 });
 
 module.exports = router;
