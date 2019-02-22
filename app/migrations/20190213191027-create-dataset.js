ef04611a2df5e65748428a07cd4aa3fa73702e00
Jeremy Clements <jeremy.clements@lexisnexisrisk.com>
2019-02-21 19:11:23 -0500

    change dataset migration to use UUID for id

--- app/migrations/20190213191027-create-dataset.js
+++ app/migrations/20190213191027-create-dataset.js
@@ -4,11 +4,7 @@ module.exports = {
     return queryInterface.createTable('Datasets', {
       id: {
         allowNull: false,
-        autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      id: {
         type: Sequelize.UUID
       },
       name: {
