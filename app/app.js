15a8ed344ff8241980f03457361a5c862e596126
Jeremy Clements <jeremy.clements@lexisnexisrisk.com>
2019-07-31 16:48:15 -0400

    add an explicit 8mb payload limit to avoid http 413s on large requests

--- app/app.js
+++ app/app.js
@@ -31,8 +31,8 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json({ limit: '8mb' }));
+app.use(express.urlencoded({ limit: '8mb', extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(cookieParser(process.env.SECRET));
