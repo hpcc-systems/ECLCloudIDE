9a30ee08fcb0bb97012d93dabe83601f5d49842a
Jeremy Clements <jeremy.clements@lexisnexisrisk.com>
2019-09-12 14:30:12 -0400

    show the username as the link to the account page, instead of "Account"

--- app/routes/index.js
+++ app/routes/index.js
@@ -5,7 +5,12 @@ const request = require('request-promise');
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.render('index', { title: 'ECL IDE' });
+  let user = {
+    id: req.session.user.id,
+    username: req.session.user.username,
+    emailVerified: req.session.user.emailVerified
+  };
+  res.render('index', { title: 'ECL IDE', user: user });
 });
 
 module.exports = router;
