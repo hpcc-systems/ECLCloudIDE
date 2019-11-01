f8bd0a0ed664eeb338a4bbe05c564713b3499c38
Jeremy Clements <jeremy.clements@lexisnexisrisk.com>
2019-11-01 12:18:28 -0400

    moving remaining directory tree functions into directoryTree module

--- app/public/javascripts/modules/directoryTree.mjs
+++ app/public/javascripts/modules/directoryTree.mjs
@@ -1,3 +1,7 @@
+'use strict';
+
+import { hostname } from './consts.mjs';
+
 let renderTree = (subtree, type = 'scripts') => {
   let $ul = $('<ul>');
   subtree.forEach((_branch) => {
@@ -68,4 +72,131 @@ let addFolder = (branch, type) => {
   return $li;
 };
 
-export { renderTree, addScript, addDataset, addFolder };
+let populateWorkspaces = () => {
+  fetch('/users/workspaces')
+    .then(response => response.json())
+    .then((workspaces) => {
+      if (workspaces.length == 0) {
+        $('.workspace-tip').removeClass('d-none');
+      } else {
+        let $workspaces = $('.workspaces');
+
+        workspaces.forEach((workspace) => {
+          console.log(workspace);
+          let $newWorkspace = $workspaces.find('.cloner').clone();
+          $newWorkspace.removeClass('d-none cloner');
+          $newWorkspace.data('name', workspace.name);
+          $newWorkspace.data('cluster', workspace.cluster);
+          $newWorkspace.data('id', workspace.id);
+          $newWorkspace.data('directoryTree', workspace.directoryTree);
+          $newWorkspace.text($newWorkspace.data('name'));
+          $workspaces.append($newWorkspace);
+        });
+      }
+    });
+};
+
+let populateWorkspaceDirectoryTree = (tree) => {
+  let datasets = tree.datasets,
+      scripts = tree.scripts,
+      $datasetsTreeRoot = $('.datasets'),
+      $scriptsTreeRoot = $('.scripts');
+
+  $datasetsTreeRoot.children('ul').remove();
+  $scriptsTreeRoot.children('ul').remove();
+
+  let $datasetsUl = renderTree(Object.entries(tree.datasets), 'datasets'),
+      $scriptsUl = renderTree(Object.entries(tree.scripts));
+
+  $datasetsUl.removeClass('d-none');
+  $scriptsUl.removeClass('d-none');
+
+  $datasetsTreeRoot.append($datasetsUl);
+  $scriptsTreeRoot.append($scriptsUl);
+};
+
+let populateDatasets = () => {
+  let url = new URL(hostname + '/datasets'),
+      $activeWorkspace = $('.workspaces .active'),
+      $datasets = $('.datasets'),
+      $folders = $datasets.find('.folder'),
+      $datasetLis = $datasets.find('.dataset:not(.cloner)'),
+      params = { workspaceId: $activeWorkspace.data('id') };
+
+  url.search = new URLSearchParams(params);
+
+  fetch(url)
+    .then(response => response.json())
+    .then((datasets) => {
+      console.log('populateDatasets', datasets);
+
+      if (Object.entries(datasets).length > 0) {
+        $datasetLis.each((idx, el) => {
+          let $dataset = $(el),
+              _dataset = datasets[$dataset.data('id')];
+          $dataset.data('query', _dataset.eclQuery);
+          $dataset.data('wuid', _dataset.workunitId);
+          $dataset.data('rows', _dataset.rowCount);
+          $dataset.data('cols', _dataset.columnCount);
+        }).promise().done(function() {
+          showDatasets();
+        });
+      }
+    });
+};
+
+let showDatasets = () => {
+  let $datasets = $('.datasets'),
+      $datasetCollapser = $('#dataset-collapser');
+
+  if (!$datasets.hasClass('show')) {
+    $datasetCollapser.trigger('click');
+  }
+};
+
+let populateScripts = () => {
+  let url = new URL(hostname + '/scripts'),
+      $activeWorkspace = $('.workspaces .active'),
+      $scripts = $('.scripts'),
+      $folders = $scripts.find('.folder'),
+      $scriptLis = $scripts.find('.script:not(.cloner)'),
+      params = { workspaceId: $activeWorkspace.data('id') };
+
+  url.search = new URLSearchParams(params);
+
+  fetch(url)
+    .then(response => response.json())
+    .then((scripts) => {
+      console.log('populateScripts', scripts);
+
+      if (Object.entries(scripts).length > 0) {
+        $scriptLis.each((idx, el) => {
+          let $script = $(el),
+              _script = scripts[$script.data('id')];
+          if (_script) {
+            $script.data('revisionId', _script.revisionId);
+            $script.data('content', _script.content);
+            $script.data('parentPathNames', _script.parentPathNames);
+            $script.data('wuid', _script.workunitId);
+          }
+        }).promise().done(function() {
+          showScripts();
+        });
+      }
+    });
+};
+
+let showScripts = () => {
+  let $scripts = $('.scripts'),
+      $scriptCollapser = $('#script-collapser');
+
+  if (!$scripts.hasClass('show')) {
+    $scriptCollapser.trigger('click');
+  }
+};
+
+export {
+  renderTree, addScript, addDataset, addFolder, populateWorkspaces,
+  populateWorkspaceDirectoryTree, populateDatasets, showDatasets,
+  populateScripts, showScripts,
+};
