1cc6583ca6760141cbe2a1ca8f43b067d3bda3c6
Jeremy Clements <jeremy.clements@lexisnexisrisk.com>
2019-11-01 12:19:13 -0400

    moving frontend hpcc workunit functions into ES6 module

---
+++ app/public/javascripts/modules/hpccWorkunits.mjs
@@ -0,0 +1,132 @@
+'use strict';
+
+import { cluster } from './consts.mjs';
+
+let createWorkunit = () => {
+  return fetch('/hpcc/workunits', {
+    method: 'POST',
+    body: JSON.stringify({
+      clusterAddr: cluster.host,
+      clusterPort: cluster.port
+    }),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  });
+};
+
+let updateWorkunit = (wuid, query, scriptName, scriptPath = null, datasetId = null, workspaceId) => {
+  return fetch('/hpcc/workunits', {
+    method: 'PUT',
+    body: JSON.stringify({
+      clusterAddr: cluster.host,
+      clusterPort: cluster.port,
+      wuid: wuid,
+      query: query,
+      filename: scriptName,
+      scriptPath: scriptPath,
+      datasetId: datasetId,
+      workspace: workspaceId
+    }),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  });
+};
+
+let submitWorkunit = (wuid) => {
+  return fetch('/hpcc/workunits/submit', {
+    method: 'POST',
+    body: JSON.stringify({
+      clusterAddr: cluster.host,
+      clusterPort: cluster.port,
+      wuid: wuid,
+      cluster: 'thor'
+    }),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  });
+};
+
+let sendFileToLandingZone = (file) => {
+  console.log(file);
+  let formData = new FormData();
+  formData.append('file', file);
+  formData.append('clusterAddr', cluster.host);
+  formData.append('clusterPort', cluster.port);
+
+  return fetch('/hpcc/filespray/upload', {
+    method: 'POST',
+    body: formData
+  });
+};
+
+let sprayFile = (clusterFilename, workspaceId) => {
+  console.log(clusterFilename);
+  let formData = new FormData();
+  formData.append('filename', clusterFilename);
+  formData.append('clusterAddr', cluster.host);
+  formData.append('clusterPort', cluster.port);
+  formData.append('workspaceId', workspaceId);
+
+  return fetch('/hpcc/filespray/spray', {
+    method: 'POST',
+    body: formData
+  });
+};
+
+let getDfuWorkunit = (wuid) => {
+  let formData = new FormData();
+  formData.append('wuid', wuid);
+  formData.append('clusterAddr', cluster.host);
+  formData.append('clusterPort', cluster.port);
+
+  return fetch('/hpcc/filespray/getDfuWorkunit', {
+    method: 'POST',
+    body: formData
+  });
+};
+
+let saveWorkunit = (objectId, workunitId) => {
+  return fetch('/workunits/', {
+    method: 'POST',
+    body: JSON.stringify({
+      objectId: objectId,
+      workunitId: workunitId
+    }),
+    headers:{
+      'Content-Type': 'application/json'
+    }
+  });
+};
+
+let checkWorkunitStatus = (wuid) => {
+  return fetch('/hpcc/workunits?wuid=' + wuid +
+    '&clusterAddr=' + encodeURIComponent(cluster.host) +
+    encodeURIComponent(':') + cluster.port);
+};
+
+let getWorkunitResults = (wuid, count, sequence) => {
+  console.log('request /hpcc/workunits/results', wuid, count);
+
+  return fetch('/hpcc/workunits/results', {
+    method: 'POST',
+    body: JSON.stringify({
+      clusterAddr: cluster.host,
+      clusterPort: cluster.port,
+      wuid: wuid,
+      count: ((count) ? count : 1000),
+      sequence: ((sequence) ? sequence : 0)
+    }),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  });
+};
+
+export {
+  createWorkunit, updateWorkunit, submitWorkunit, sendFileToLandingZone,
+  sprayFile, getDfuWorkunit, saveWorkunit, checkWorkunitStatus,
+  getWorkunitResults,
+};
