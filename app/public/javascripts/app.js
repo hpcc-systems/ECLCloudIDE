7850b94f21c4559bb7e46ad7a6f049561cf79388
Jeremy Clements <jeremy.clements@lexisnexisrisk.com>
2019-11-01 12:13:06 -0400

    move shepherd js snippet into ES6 module

--- app/public/javascripts/app.js
+++ app/public/javascripts/app.js
@@ -3,6 +3,8 @@
 let hostname = window.location.protocol + '//' + window.location.hostname +
   (window.location.port ? (':' + window.location.port) : '');
 
+import { tour } from './modules/featureTour.mjs';
+
 const NO_WORKSPACE = 'Select Workspace...';
 const NEW_SCRIPT = 'New Script...';
 const NEW_DATASET = 'New Dataset...';
@@ -450,76 +452,6 @@ require([
       $outputsPanel.css('width', 'calc(100% - ' + $sidebar.css('width') + ')');
       $scriptPanel.css('width', 'calc(100% - ' + $sidebar.css('width') + ')');
     }
-  });
-
-  let tour = new Shepherd.Tour({
-    defaultStepOptions: {
-      classes: 'shadow-md bg-purple-dark',
-      scrollTo: { behavior: 'smooth', block: 'center' },
-      showCancelLink: true,
-      when: {
-        show: function() {
-          $('.shepherd-element').find('video').trigger('play');
-        },
-        hide: function() {
-          $('.shepherd-element').find('video').trigger('pause');
-        }
-      },
-      shepherdElementMaxWidth: '500px'
-    },
-    useModalOverlay: true,
-    steps: [{
-      id: 'workspace-step',
-      title: 'Create a Workspace',
-      text: `
-        First, create a new workspace to contain your<br />uploaded data files and ECL scripts.<br /></br />
-        <video muted autoplay loop width="600">
-          <source src="/videos/create-workspace.webm" type="video/webm" />
-          An HTML5 video tag showing the creation of a workspace.
-        </video>
-      `,
-      attachTo: {
-        element: '.navbar-nav',
-      },
-      buttons: [
-        { text: 'Exit', secondary: true, action: function() { return this.cancel(); } },
-        { text: 'Next', secondary: false, action: function() { return this.next(); } }
-      ]
-    }, {
-      id: 'datasets-step',
-      title: 'Upload a CSV',
-      text: `
-        Then upload a CSV file containing some data. The first<br />row of the file should contain column headings.<br /><br />
-        <video muted autoplay loop width="600">
-          <source src="/videos/add-dataset.webm" type="video/webm" />
-          An HTML5 video tag showing uploading a CSV dataset.
-        </video>
-      `,
-      attachTo: {
-        element: '#datasets-wrapper'
-      },
-      buttons: [
-        { text: 'Back', secondary: true, action: function() { return this.back(); } },
-        { text: 'Next', secondary: false, action: function() { return this.next(); } }
-      ]
-    }, {
-      id: 'scripts-step',
-      title: 'Create an ECL Script',
-      text: `
-        Create a new script, add some ECL, and run that code<br />on the Workspace's cluster.<br /><br />
-        <video muted autoplay loop width="600">
-          <source src="/videos/add-script.webm" type="video/webm" />
-          An HTML5 video tag showing creating and executing an ECL script.
-        </video>
-      `,
-      attachTo: {
-        element: '#scripts-wrapper'
-      },
-      buttons: [
-        { text: 'Back', secondary: true, action: function() { return this.back(); } },
-        { text: 'Done', secondary: false, action: function() { return this.next(); } }
-      ]
-    }]
   });
 
   $('#tour-link').on('click', function() {
