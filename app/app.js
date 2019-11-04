105ea43c82f8ab37bfafbb447bd73ac012e39ed7
Jeremy Clements <jeremy.clements@lexisnexisrisk.com>
2019-11-04 17:52:42 -0500

    need to set the trust proxy in express so that secure cookies work properly

--- app/app.js
+++ app/app.js
@@ -31,6 +31,8 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+app.set('trust proxy', ['uniquelocal']);
+
 app.use(compression());
 
 app.use(logger('dev'));
