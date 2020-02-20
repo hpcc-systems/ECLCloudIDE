0dbaa5f4291a5fa5cddb1faf44d818ce47845a9e
Jeremy Clements <jeremy.clements@lexisnexisrisk.com>
2020-02-20 12:18:49 -0500

    change the nginx service to use an entrypoint instead of command, so that we can pass the hostname in

--- nginx/run_nginx.sh
+++ nginx/run_nginx.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 echo "######## Run nginx"
+hostname="$1"
+export HOSTNAME=$hostname
 export DOLLAR='$'
 envsubst < /etc/nginx/conf.d/eclide.conf.template > /etc/nginx/conf.d/eclide.conf
 nginx -g "daemon off;"
