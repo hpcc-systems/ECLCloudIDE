b3c51afafa7fa4cef296de7b55fd6370bb1aee74
Jeremy Clements <jeremy.clements@lexisnexisrisk.com>
2020-03-11 17:00:20 -0400

    add a VM entry to development clusters

--- app/cluster-whitelist.js
+++ app/cluster-whitelist.js
@@ -1,5 +1,6 @@
 module.exports = {
   "development": [
+    "http://192.168.99.102:8010",
     "http://10.173.147.1:8010",
     "http://play.hpccsystems.com:8010",
     "http://alpha_dev_thor_esp.risk.regn.net:8010",
